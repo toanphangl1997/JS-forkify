5137aba9e1174343fc2d9bef7dc20e7c5b24d1cf
toanphangl1997 <toanphangl1997@gmail.com>
2025-01-20 22:34:30 +0700

    forkify

---
+++ README.txt
@@ -0,0 +1,6 @@
+"dependencies": {
+    "core-js": "^3.40.0",
+    "fractional": "^1.0.0",
+    "regenerator-runtime": "^0.14.1",
+    "sass": "^1.83.4"
+  }
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '19fee9fd-efc0-4de0-bb90-6e32c3a2efa5';
+export const MODAL_CLOSE_SEC = 2.5;
